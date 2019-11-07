be8df17d75101ca850760aedaa41a30b23b280f9
Andries Smit <mr.andries.smit@gmail.com>
2019-11-07 12:56:22 +0100

    Action are always optional, can be set Do nothing

--- src/functions.ts
+++ src/functions.ts
@@ -340,7 +340,7 @@ export const transformJsonContent = (jsonContent: MendixXML, widgetName: string)
         ? properties
               .map(prop => {
                   let name = prop.$.key;
-                  if (prop.$.required && prop.$.required === "false" && prop.$.type !== "object") {
+                  if (prop.$.required && prop.$.required === "false" && prop.$.type !== "object" || prop.$.type !== "action" ) {
                       name += "?";
                   }
                   const type = translateType(prop, childTypes, true, mobile);
